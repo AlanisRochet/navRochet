69a6250c0f18980e20d01da97c6126b71d02217d
Alanis Rochet <alanis.brend@gmail.com>
2022-06-22 23:57:50 -0300

    modif

---
+++ General/taza-app/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import NavBar from './components/NavBar'
+
+
+
+function App() {  
+
+	return (   
+
+		 <div className="App">     
+
+			<Navbar></Navbar>
+
+		</div>  
+
+	);
+
+}
+
+export default App;
